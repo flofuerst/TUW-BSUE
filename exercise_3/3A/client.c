4f14a0478acf33b9b98f9f2a548c570c860c61aa
12122096 <e12122096@student.tuwien.ac.at>
2023-01-14 18:18:25 +0100

    fixed exercise 3A

--- exercise_3/3A/client.c
+++ exercise_3/3A/client.c
@@ -1,3 +1,15 @@
+/**
+ * @file client.c
+ * @author Florian Fürst (12122096)
+ * @brief Client program which uses HTTP 1.1. The client can take an URL as input, connects to the corresponding server
+ * and also requests the files specified in the URL. The transmitted data can be written to stdout or to file. 
+ * It is also possible to specify a directory of a file, where the transmitted data should be written to. 
+ * @version 0.1
+ * @date 2023-01-14
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -12,14 +24,31 @@
 #include <sys/types.h>
 #include <netdb.h>
 
+/**
+ * @brief the program name which is used for usage and error messages
+ * 
+ */
 static char *argv_name;
 
+/**
+ * @brief Prints the correct usage(synopsis) of the program to stderr
+ * 
+ */
 static void usage(void)
 {
     fprintf(stderr, "SYNOPSIS:\n%s [-p PORT] [ -o FILE | -d DIR ] URL\n", argv_name);
     exit(EXIT_FAILURE);
 }
 
+/**
+ * @brief Checks if the given url is correct and splits the url into the host and request parts. Corrects
+ * the request string/part if it is NULL or doesn't start with an '/'. Also checks if hostname is valid (non-empty).
+ * 
+ * @param url The whole url which is given through input
+ * @param host The host of the url, which begins after the "http://" and ends before the first occurence of one of the 
+ * following characters ';/?:@=&'.
+ * @param request The request part of the url. If it starts with an '?', a '/' gets added at the front.
+ */
 static void parse_url(char *url, char *host, char *request)
 {
     // check if url starts with http://
@@ -43,28 +72,28 @@ static void parse_url(char *url, char *host, char *request)
         request[1] = '\0';
 
         strcpy(host, host_total);
-        // printf("host: %s\n", host);
-        // printf("request: %s\n", request);
     }
     // If character(s) above exist: copy all characters after first occurrence to request string
     else
     {
-        strcpy(request, request_temp);
+        int corrected = 0;
+        // add / in front of request if not existing
+        if (request_temp[0] == '?')
+        {
+            request[0] = '/';
+            request[1] = '\0';
+            strcat(request, request_temp);
+            corrected = 1;
+        }
+        else
+            strcpy(request, request_temp);
 
-        // printf("%s\n", host_total);
-        // printf("request: %s\n", request);
-        // printf("request[0]: %c\n", request[0]);
-
-        char *e = strchr(host_total, request[0]);
-        // printf("%s\n", e);
+        // index corrected is used to index the correct first element of request
+        //(if / was missing in front, index 0 would be false)
+        char *e = strchr(host_total, request[corrected]);
         int index = (int)(e - host_total);
-        // printf("%d\n", index);
-        // printf("%c\n", host_total[index]);
-
         strncpy(host, host_total, index);
-        // printf("%s\n", host);
         host[index] = '\0';
-        // printf("host: %s\n", host);
     }
 
     // check if invalid hostname
@@ -75,7 +104,14 @@ static void parse_url(char *url, char *host, char *request)
     }
 }
 
-static int socket_setup(char *host, char *request, char *port)
+/**
+ * @brief Setup for the server connection. Returns the given file decriptor which is needed for further implementation.
+ * 
+ * @param host The host which is used by getaddrinfo
+ * @param port The port which is used by getaddrinfo
+ * @return int Returns the file descriptor of the socket
+ */
+static int socket_setup(char *host, char *port)
 {
     struct addrinfo hints, *ai;
     memset(&hints, 0, sizeof hints);
@@ -106,6 +142,13 @@ static int socket_setup(char *host, char *request, char *port)
     return socket_fd;
 }
 
+/**
+ * @brief Reads the header, parses its content and checks if header is valid. Prints error messages and 
+ * returns appropriate exit code
+ * 
+ * @param sockfile The file which is used to read the content
+ * @return int Returns the appropriate exit code of the program. If everything is fine, then exit code 0 is returned
+ */
 static int validate_response_header(FILE *sockfile)
 {
     char *line = NULL;
@@ -124,10 +167,10 @@ static int validate_response_header(FILE *sockfile)
     char *rest = NULL;
     version = strtok_r(line, " ", &rest);
     code = strtok_r(NULL, " ", &rest);
-    message = strtok_r(NULL, " ", &rest);
-    fprintf(stderr, "http-version=%s status=%s text-status=%s\n", version, code, message);
+    message = strtok_r(NULL, "\r", &rest);
 
-    if (strncmp(version, "HTTP/1.1", strlen("HTTP/1.1")) != 0)
+    char *end_char;
+    if (strncmp(version, "HTTP/1.1", strlen("HTTP/1.1")) != 0 || code == NULL || strtol(code, &end_char, 10) == 0)
     {
         fprintf(stderr, "%s Protocol error!\n", argv_name);
         free(line);
@@ -150,16 +193,31 @@ static int validate_response_header(FILE *sockfile)
     return 0;
 }
 
-static void read_body_content(FILE* output, FILE* sockfile){
+/**
+ * @brief Reads the body content of the reponse and writes this content to the output file
+ * 
+ * @param output The output file where the content gets written to. Can be stdout or a file
+ * @param sockfile The file which is used to read the content
+ */
+static void read_body_content(FILE *output, FILE *sockfile)
+{
     uint8_t buf[1024];
     size_t read;
 
-    while((read = fread(buf, sizeof(uint8_t), 1024, sockfile))!= 0){
+    while ((read = fread(buf, sizeof(uint8_t), 1024, sockfile)) != 0)
+    {
         fwrite(buf, sizeof(uint8_t), read, output);
     }
     fflush(output);
 }
 
+/**
+ * @brief Reads the input and checks if the program is called correctly and handles the whole process of this exercise
+ * 
+ * @param argc number of arguments of the program call
+ * @param argv array of the arguments of the program call
+ * @return int return value of main method to tell when program is finished (and therefore can be removed from memory)
+ */
 int main(int argc, char *argv[])
 {
     argv_name = argv[0];
@@ -175,7 +233,8 @@ int main(int argc, char *argv[])
 
     if (argc <= 1)
     {
-        usage();
+        fprintf(stdout, "SYNOPSIS:\n%s [-p PORT] [ -o FILE | -d DIR ] URL\n", argv_name);
+        exit(EXIT_FAILURE);
     }
 
     int c;
@@ -231,7 +290,6 @@ int main(int argc, char *argv[])
     char request[strlen(url)];
 
     parse_url(url, host, request);
-    // printf("host: %s | request: %s\n", host, request);
 
     // write to stdout if no filename or directory specified
     if (!file_set && !dir_set)
@@ -247,21 +305,34 @@ int main(int argc, char *argv[])
             filepath = filename;
 
         // set filepath to dirname/filename if directory specified
+
+        char* filenameOriginal;
         if (dir_set)
         {
-            filename = request + 1;
-            if (filename[0] == '\0')
+            filenameOriginal = strdup(request + 1);
+            filename = filenameOriginal;
+
+            if (filename[0] == '\0' || filename[0] == '?')
             {
                 filename = "index.html";
             }
-            filepath = strdup(filename);
-            strcpy(filepath, dirname);
+            else
+            {
+                filename = strtok(filename, "?");
+            }
+
+            filepath = strdup(dirname);
             strcat(filepath, "/");
             strcat(filepath, filename);
+        }
+        output = fopen(filepath, "w");
+
+        // free filepath if directory is set (because of strdup)
+        if (dir_set)
+        {
+            free(filenameOriginal);
             free(filepath);
         }
-
-        output = fopen(filepath, "w");
 
         if (output == NULL)
         {
@@ -271,7 +342,7 @@ int main(int argc, char *argv[])
     }
 
     // set up connection
-    int socket_fd = socket_setup(host, request, port);
+    int socket_fd = socket_setup(host, port);
     if (socket_fd < 0)
     {
         fprintf(stderr, "%s Error, while setting up socket: %s\n", argv_name, strerror(errno));
